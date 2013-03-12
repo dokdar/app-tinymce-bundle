edf646af855d27d2088de15d865b131259f21d44
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2013-03-12 07:08:59 +0100

    wip

---
+++ src/Gajdaw/SimpleMenuBundle/Form/MenuType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Gajdaw\SimpleMenuBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class MenuType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('contents')
+            ->add('slug')
+        ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Gajdaw\SimpleMenuBundle\Entity\Menu'
+        ));
+    }
+
+    public function getName()
+    {
+        return 'gajdaw_simplemenubundle_menutype';
+    }
+}
